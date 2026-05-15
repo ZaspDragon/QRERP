db521c5e7962aecb84e5af0d42290d67f96617b6
ZaspDragon <ilevanshine@gmail.com>
2026-05-15 15:36:19 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./",
+  base: "/QR-Warehouse-ERP/",
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
 });
