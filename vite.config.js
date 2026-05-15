90515f8502ce0db9131f50ec455c43b98ef8f2ae
ZaspDragon <ilevanshine@gmail.com>
2026-05-15 15:37:54 -0400

    Change base path in Vite config to relative

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/QR-Warehouse-ERP/",
+  base: "./",
   plugins: [react()],
 });
